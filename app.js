95ff0056976a4db9b21b581e70480842cc73e546
Matthew Bisicchia <matthewbisicchia@gmail.com>
2022-01-30 01:00:27 -0500

    Implement boiler-plate Express App

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// Create an Express app
+const app = express();
+
+// Define a callback function to respond to client's get request at '/'
+app.get('/', (req, res) => {
+    res.send("Website of Matthew Bisicchia");
+})
+
+// Define callback function to respond to client's request;
+// client will attempt to connect to the web server (to be hosted on AWS)
+// through port 3000
+app.listen(3000, () => {
+    console.log("Website of Matthew Bisicchia");
+});
