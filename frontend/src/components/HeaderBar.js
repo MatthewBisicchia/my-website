24ee49e7d36b7db7baaee0469a5a54240f952930
Matthew Bisicchia <matthewbisicchia@gmail.com>
2022-03-18 21:23:20 -0400

    Initial implementation of frotnend for home page

---
+++ frontend/src/components/HeaderBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './styles/HeaderBar.module.css';
+
+const HeaderBar = () => {
+
+    return(
+        <header id={styles.header}>
+
+            matthewbisicchia.com
+
+        </header>
+
+
+    );
+}
+
+export default HeaderBar;
