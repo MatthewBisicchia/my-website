4030105f5488832c7a655ade0da4323090202a54
Matthew Bisicchia <you@example.com>
2022-05-22 19:48:46 -0400

    Refactor StartNewLab fields for dynamic component rendering

--- biologists_dashboard/src/components/StartNewLab/StartNewLab.js
+++ biologists_dashboard/src/components/StartNewLab/StartNewLab.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import styles from './StartNewLab.module.css';
+import StartNewLabFormTextField from  './StartNewLabFormTextField';
 
 
 /*  TO DO: Transition this into a modal. Along with Create Issue will be a modal.
 Everything else will be within the outlet within the dashboard. */
+
+const fields = [
+    {
+        label: 'Name', 
+    },
+    {
+        label: 'Type', 
+    },
+    {
+        label: 'Details', 
+    },
+    {
+        label: 'Toxic Chemicals', 
+    },
+    {
+        label: 'Steps', 
+    },
+    {
+        label: 'Connect Devices', 
+    },
+    {
+        label: 'Automate Jobs/Processes', 
+    },
+
+]
 
 const StartNewLab = () => {
 
@@ -19,45 +45,13 @@ const StartNewLab = () => {
                 <hr>
                 </hr>
 
-
-                <div className={styles.field}>
-                    <div className={styles.fieldLabel}>Name:</div>
-                    <div className={styles.fieldInput}><input style={{height: '25px', width: '500px'}}></input></div>
-                </div>
-
-                <div className={styles.field}>
-                    {/*should be a drop down*/}
-                    <div className={styles.fieldLabel}>Type:</div>
-                    <div className={styles.fieldInput}><input style={{height: '25px', width: '500px'}}></input></div>
-                </div>
-
-                <div className={styles.field}>
-                    <div className={styles.fieldLabel}>Details:</div>
-                    <div className={styles.fieldInput}><input style={{height: '250px', width: '500px'}}></input></div>
-                </div>
-
-                <div className={styles.field}>
-                    {/*should be a drop down, will generate safety concerns*/}
-                    <div className={styles.fieldLabel}>Toxic Chemicals:</div>
-                    <div className={styles.fieldInput}><input style={{height: '25px', width: '500px'}}></input></div>
-                </div>
-
-                <div className={styles.field}>
-                {/*Perhaps create a TO DO list, with ability to add items, so that you can have an automated checklist*/}
-                <div className={styles.fieldLabel}>Steps:</div>
-                <div className={styles.fieldInput}><input style={{height: '25px', width: '500px'}}></input></div>
-                </div>
-
-                <div className={styles.field}>
-                    <div className={styles.fieldLabel}>Connect Devices:</div>
-                    <div className={styles.fieldInput}><input style={{height: '25px', width: '500px'}}></input></div>
-                </div>
-
-                <div className={styles.field}>
-                    {/*should be a drop down*/}
-                    <div className={styles.fieldLabel}>Automate Jobs/Processes:</div>
-                    <div className={styles.fieldInput}><input style={{height: '25px', width: '500px'}}></input></div>
-                </div>
+                {
+                    fields.map(field => {
+                        return ([
+                            <StartNewLabFormTextField field={field} />
+                        ]);
+                    })
+                }
 
                 <div style={{display: 'flex', alignItems: 'center', textAlign: 'center', justifyContent: 'center'}}>
                     <div id={styles.startLabButton}>
