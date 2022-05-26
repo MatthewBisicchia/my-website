aff812ea7755499c48d2c0b2ac344adde25b9232
Matthew Bisicchia <you@example.com>
2022-05-26 02:28:25 -0400

    Fix axios API issue in client side

--- biolab/biologists_dashboard/src/api/index.js
+++ biolab/biologists_dashboard/src/api/index.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-const url_getAllLabs = 'http://matthewbisicchia.com/biolab_api/labs/getAllLabs';
-const url_postLab = 'http://matthewbisicchia.com/biolab_api/labs/createNewLab';
+// const url_getAllLabs = 'http://matthewbisicchia.com/biolab_api/labs/getAllLabs';
+// const url_postLab = 'http://matthewbisicchia.com/biolab_api/labs/createNewLab';
+
+const url_getAllLabs = 'http://localhost:3000/biolab_api/labs/getAllLabs';
+const url_postLab = 'http://localhost:3000/biolab_api/labs/createNewLab';
 
 export const getLabs = () => axios.get(url_getAllLabs);
 
