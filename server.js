e2cfe4678f9ec4b39ef4b144c27479236961dd8a
Matthew Bisicchia <you@example.com>
2022-05-26 13:53:18 -0400

    Add origin true to CORS

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Enable Cross Origin Resource Sharing
-app.use(cors());
+app.use(cors({origin: true}));
 
 // Load build of the Main Site:
 app.use('/mainSite', express.static(path.join(__dirname, 'frontend/build')))
