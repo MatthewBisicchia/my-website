a7019303a37f03787cbadf1d6979103686d4d491
Matthew Bisicchia <you@example.com>
2022-05-26 14:14:57 -0400

    Add CORS headers to axios API calls in BioLab frontend

--- biolab/biologists_dashboard/src/api/index.js
+++ biolab/biologists_dashboard/src/api/index.js
@@ -1,10 +1,26 @@
 import axios from 'axios';
+import React, { useEffect } from 'react';
 
 const url_getAllLabs = 'http://www.matthewbisicchia.com/biolab_api/labs/getAllLabs';
 const url_postLab = 'http://www.matthewbisicchia.com/biolab_api/labs/createNewLab';
 
 // const url_getAllLabs = 'http://localhost:3000/biolab_api/labs/getAllLabs';
 // const url_postLab = 'http://localhost:3000/biolab_api/labs/createNewLab';
+
+const url_main = "http://www.matthewbisicchia.com";
+
+const corsOptions = {
+   url_main,
+   headers: {
+    'Access-Control-Allow-Origin' : 'http://www.matthewbisicchia.com',
+    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH',
+    }
+}
+
+axios(corsOptions)
+.then(response => {
+    setData(response);
+}) 
 
 export const getLabs = () => axios.get(url_getAllLabs);
 
