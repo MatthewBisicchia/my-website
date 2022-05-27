2f88d320dc9d70541581daed2101147b190cc1a9
Matthew Bisicchia <matthewbisicchia@gmail.com>
2022-05-26 22:52:46 -0400

    Update AWS environment variable configuration for database connection

--- biolab/biologists_dashboard_backend/database/index.js
+++ biolab/biologists_dashboard_backend/database/index.js
@@ -1,24 +1,26 @@
 import mysql from 'mysql';
-import cryptojs from "crypto-js";
+//import cryptojs from "crypto-js";
 
-let decrypt_ = cryptojs.AES.decrypt(process.env.AWS_CONFIGURATION_VAR_2, process.env.AWS_CONFIGURATION_VAR_1);
-let decrypt_string_ = decrypt_.toString(cryptojs.enc.Utf8)
+//let decrypt_ = cryptojs.AES.decrypt(process.env.AWS_CONFIGURATION_VAR_2, process.env.AWS_CONFIGURATION_VAR_1);
+//let decrypt_string_ = decrypt_.toString(cryptojs.enc.Utf8)
 
 // Create connection to BioLab MySQL database:
 const database = mysql.createConnection({
     host: process.env.AWS_CONFIGURATION_VAR_5,
     port: process.env.AWS_CONFIGURATION_VAR_4,
     user: process.env.AWS_CONFIGURATION_VAR_3,
-    password: decrypt_string_,
-    database: process.env.AWS_CONFIGURATION_VAR_6
+    password: process.env.AWS_CONFIGURATION_VAR_2,
+    database: process.env.AWS_CONFIGURATION_VAR_1
 });
 
-console.log(process.env.AWS_CONFIGURATION_VAR_6);
+console.log("TESTING ENVIRONMENT VARIABLE:")
+console.log(process.env.AWS_CONFIGURATION_VAR_1);
 
 // Connect to BioLab MySQL database:
 database.connect( (error) => {
     if(error) 
     {
+        console.log("COULD NOT CONNECT TO DATABASE");
         console.log(error);
     }
     else 
