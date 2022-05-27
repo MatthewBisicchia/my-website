367b4e4743ae386b32dca2c4eae045008c189491
Matthew Bisicchia <matthewbisicchia@gmail.com>
2022-05-27 00:26:48 -0400

    Update Axios settings

--- biolab/biologists_dashboard/src/api/index.js
+++ biolab/biologists_dashboard/src/api/index.js
@@ -7,21 +7,21 @@ const url_postLab = 'http://www.matthewbisicchia.com/biolab_api/labs/createNewLa
 // const url_getAllLabs = 'http://localhost:3000/biolab_api/labs/getAllLabs';
 // const url_postLab = 'http://localhost:3000/biolab_api/labs/createNewLab';
 
-// const url_main = "http://www.matthewbisicchia.com";
+//const url_main = "http://www.matthewbisicchia.com";
 
-// const corsOptions = {
-//    url_main,
-//    headers: {
-//     'Access-Control-Allow-Origin' : 'http://www.matthewbisicchia.com/*',
-//     'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH',
-//     }
-// }
+const configuration = {
+   headers: {
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH',
+    }
+}
 
 // axios(corsOptions)
 // .then(response => {
 //     setData(response);
+//     console.log('AXIOS CORS SETTINGS UPDATED');
 // }) 
 
-export const getLabs = () => axios.get(url_getAllLabs);
+export const getLabs = () => axios.get(url_getAllLabs, configuration);
 
-export const createLab = (createdLab) => axios.post(url_postLab, createdLab);
+export const createLab = (createdLab) => axios.post(url_postLab, createdLab, configuration);
